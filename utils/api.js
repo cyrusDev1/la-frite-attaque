a7911cae8a6f2e5a4b7616e0f0f499c727fdd535
cyrusDev1 <cyrushoungue1240@gmail.com>
2025-05-22 10:36:17 +0100

    replace api url

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://api.lafriteattaque.fr";
 
 export async function get(endpoint) {
   const res = await fetch(`${BASE_URL}/${endpoint}`);
